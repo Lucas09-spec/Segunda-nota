880e4ed91dcbc23216e500b82aab968f439669be
benjazzx <ben.aranedas@duocuc.cl>
2025-05-26 21:11:59 -0400

    Delete Direccion directory

--- Direccion/Direccion/src/main/java/com/Direccion/Repository/ComRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Direccion.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Direccion.Model.Comuna;
-
-public interface ComRepository extends JpaRepository <Comuna,Long> {
-
-}
